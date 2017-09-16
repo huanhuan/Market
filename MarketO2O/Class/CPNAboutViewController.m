55e0933f58cfd127c9926b8929d332f97bfa8a0c
上海－彭欢欢 <penghuanhuan@jd.com>
2017-09-16 10:09:51 +0800

    关于我们

--- MarketO2O/Class/CPNAboutViewController.m
+++ MarketO2O/Class/CPNAboutViewController.m
@@ -11,7 +11,8 @@
 @interface CPNAboutViewController ()
 
 @property (nonatomic, strong)UIImageView *logoImage;
-@property (nonatomic, strong)UILabel *versionLabel;
+@property (nonatomic, strong)CPNCommonLabel *versionLabel;
+@property (nonatomic, strong)CPNCommonLabel *copyRightLabel;
 
 
 @end
@@ -23,27 +24,51 @@
     self.title = @"关于我们";
     
     self.logoImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"appIcon"]];
-    self.logoImage.frame = CGRectMake(0, 0, 90, 90);
-    self.logoImage.centerX = self.view.centerX;
-    self.logoImage.centerY = 80;
+//    self.logoImage.frame = CGRectMake(0, 0, 90, 90);
+//    self.logoImage.centerX = self.view.centerX;
+//    self.logoImage.centerY = 80;
     [self.view addSubview:self.logoImage];
+    [self.logoImage mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.and.height.mas_equalTo(90);
+        make.centerX.equalTo(self.view);
+        make.top.mas_equalTo(self.view.top).offset(20);
+    }];
     
-    self.versionLabel = [UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, <#CGFloat height#>)
+    [self.versionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(MAIN_SCREEN_WIDTH);
+        make.top.equalTo(self.logoImage.mas_bottom).offset(20);
+    }];
+    
+    [self.copyRightLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(MAIN_SCREEN_WIDTH);
+        make.bottom.equalTo(self.view.mas_bottom).offset(-20);
+    }];
+//    self.versionLabel setTextColor:COLOR_BLACK
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (CPNCommonLabel *)versionLabel
+{
+    if (!_versionLabel) {
+        _versionLabel = [CPNCommonLabel commonLabelWithTitle:[NSString stringWithFormat:@"%@ %@", CPN_APP_NAME, CPN_APP_VERSION]
+                                                     textFont:CPNCommonFontFifteenSize
+                                                    textColor:CPNCommonMaxDarkGrayColor];
+        _versionLabel.numberOfLines = 1;
+        _versionLabel.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:_versionLabel];
+    }
+    return _versionLabel;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (CPNCommonLabel *)copyRightLabel
+{
+    if (!_copyRightLabel) {
+        _copyRightLabel = [CPNCommonLabel commonLabelWithTitle:@"Copyright©2017\n深圳市润隆实业有限公司版权所有"
+                                                    textFont:CPNCommonFontFifteenSize
+                                                   textColor:CPNCommonMaxDarkGrayColor];
+        _copyRightLabel.numberOfLines = 0;
+        _copyRightLabel.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:_copyRightLabel];
+    }
+    return _copyRightLabel;
 }
-*/
 
 @end
